53bfc8e6f163a4bba4d4cf6d5c863bf86ec15032
A1r-max <2305491@escolas.anchieta.br>
2024-10-23 21:56:45 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -8,7 +8,7 @@ st.title("MEU 1º WEB APP STREAMLIT")
 st.header("seja bem vindo")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("https://www.google.com/url?sa=i&url=https%3A%2F%2Ftecnoblog.net%2Fresponde%2Fcomo-pesquisar-imagem-no-google-pelo-celular-android-e-ios%2F&psig=AOvVaw3SKYqlCvM8eWaJel5twT2q&ust=1729817719192000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPiLq-nmpYkDFQAAAAAdAAAAABAE")
+st.subheader("")
 
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
